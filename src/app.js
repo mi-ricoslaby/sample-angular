21294cea65e75997ff4459715c072844d76e93af
Daniel Weise <daniel.weise@movingimage.com>
2016-01-19 13:05:02 +0100

    Resource Builder

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 'use strict';
 
 var appName = 'sample-angular';
+var angular = require('angular');
 
-angular.module(appName, []);
+require('angular-resource');
+require('mi-angular-resource-builder');
+
+var requires = [
+    'ngResource',
+    'mi.ResourceBuilder'
+];
+
+angular.module(appName, requires);
 
 angular.bootstrap(document, [appName]);
